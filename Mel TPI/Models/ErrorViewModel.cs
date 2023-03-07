262c1e4c6486e72818f4177809ba09566bc149e1
ac111953 <ac111953@8CG8163KZ1>
2023-03-08 12:01:28 +1300

    Add project files.

---
+++ Mel TPI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Mel_TPI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
