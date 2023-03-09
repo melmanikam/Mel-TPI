7b5703fd1f767c62dc700a2bc7e97f3841dcabeb
Mel Manikam <ac111953@avcol.school.nz>
2023-03-10 10:10:55 +1300

    Added tab to navbar, changed privacy policy to about us page

--- Mel TPI/Controllers/HomeController.cs
+++ Mel TPI/Controllers/HomeController.cs
@@ -18,7 +18,12 @@ namespace Mel_TPI.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
         {
             return View();
         }
