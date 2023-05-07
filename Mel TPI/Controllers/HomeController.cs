b4b7d2cda048f794ebbc5f3a95a53cade94aa367
Mel Manikam <ac111953@avcol.school.nz>
2023-05-07 15:15:31 +1200

    Made the contact us form submit button work and changed booking form so theres more options

--- Mel TPI/Controllers/HomeController.cs
+++ Mel TPI/Controllers/HomeController.cs
@@ -1,42 +1,68 @@
 ﻿using Mel_TPI.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.Scripting;
 using System.Diagnostics;
 
 namespace Mel_TPI.Controllers
 {
-    public class HomeController : Controller
-    {
-        private readonly ILogger<HomeController> _logger;
+	public class HomeController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
+		public HomeController(ILogger<HomeController> logger)
+		{
+			_logger = logger;
+		}
 
-        public IActionResult Index()
-        {
-            return View();
-        }
+		public IActionResult Index()
+		{
+			return View();
+		}
 
-        public IActionResult About()
-        {
-            return View();
-        }
+		public IActionResult About()
+		{
+			return View();
+		}
 
-        public IActionResult Fees()
-        {
-            return View();
-        }
+		public IActionResult Fees()
+		{
+			return View();
+		}
 
-        public IActionResult Contact()
-        {
-            return View();
-        }
+		public IActionResult Contact()
+		{
+			return View();
+		}
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
-    }
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+
+		[HttpPost]
+		public ActionResult contactForm(string name, string email, string message)
+		{
+			ViewBag.Name = name;
+			ViewBag.Email = email;
+			ViewBag.Message = message;
+
+			return View("Contact");
+		}
+		[HttpPost]
+		public ActionResult booking(string name, string email, string phone, string address, string suburb, string city, string zip, string level, string lesson)
+		{
+			ViewBag.Name = name;
+			ViewBag.Email = email;
+			ViewBag.Phone = phone;
+			ViewBag.Address = address;
+			ViewBag.Suburb = suburb;
+			ViewBag.City = city;
+			ViewBag.Zip = zip;
+			ViewBag.Level = level;
+			ViewBag.Lesson = lesson;
+
+			return View("Index");
+		}
+	}
 }
---
+++ Mel TPI/Models/Contact.cs
@@ -0,0 +1,10 @@
+﻿namespace Mel_TPI.Models
+{
+    public class Contact
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Message { get; set; }
+
+    }
+}
