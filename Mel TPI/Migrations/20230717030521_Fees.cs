882fd3426dbd8773760eb516a8b42eef1ac3d87f
Mel Manikam <ac111953@avcol.school.nz>
2023-07-24 14:32:52 +1200

    login

---
+++ Mel TPI/Migrations/20230717030521_Fees.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Mel_TPI.Migrations
+{
+    public partial class Fees : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
--- Mel TPI/Program.cs
+++ Mel TPI/Program.cs
@@ -2,39 +2,65 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Mel_TPI.Data;
 using Mel_TPI.Areas.Identity.Data;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 
-var builder = WebApplication.CreateBuilder(args);
-var connectionString = builder.Configuration.GetConnectionString("Mel_TPIContextConnection") ?? throw new InvalidOperationException("Connection string 'Mel_TPIContextConnection' not found.");
-
-builder.Services.AddDbContext<Mel_TPIContext>(options =>
-    options.UseSqlServer(connectionString));
-
-builder.Services.AddDefaultIdentity<Mel_TPIUser>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddEntityFrameworkStores<Mel_TPIContext>();
-
-// Add services to the container.
-builder.Services.AddControllersWithViews();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+namespace Mel_TPI
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var connectionString = builder.Configuration.GetConnectionString("Mel_TPIContextConnection") ?? throw new InvalidOperationException("Connection string 'Mel_TPIContextConnection' not found.");
+
+            builder.Services.AddDbContext<Mel_TPIContext>(options =>
+                options.UseSqlServer(connectionString));
+
+            builder.Services.AddIdentity<Mel_TPIUser, IdentityRole>(options => options.SignIn.RequireConfirmedAccount = false)
+                .AddRoles<IdentityRole>()
+                .AddEntityFrameworkStores<Mel_TPIContext>();
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+
+            app.UseRouting();
+            app.UseAuthentication(); ;
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+            app.MapRazorPages();
+
+            using (var scope = app.Services.CreateScope())
+            {
+                var roleManager = scope.ServiceProvider.GetRequiredService<UserManager<IdentityRole>>();
+                var roles = new[] { "Admin", "Student" };
+
+                foreach (var role in roles)
+                {
+                    if (!await roleManager.FindByEmailAsync(role))
+                        await role
+                }
+            }
+                app.Run();
+
+        }
+    }
 }
 
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-app.UseAuthentication();;
-
-app.UseAuthorization();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-app.MapRazorPages();
-app.Run();
