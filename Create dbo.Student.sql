9bc1fefa25148be4ddf2b50b96cf8f378e20580b
Mel Manikam <ac111953@avcol.school.nz>
2023-08-23 11:37:46 +1200

    added values for students database

--- Create dbo.Student.sql
+++ Create dbo.Student.sql
@@ -1,27 +1,9 @@
-﻿USE [Mel TPI]
-GO
-
-/****** Object: Table [dbo].[Student] Script Date: 27/07/2023 10:58:00 a.m. ******/
-SET ANSI_NULLS ON
-GO
-
-SET QUOTED_IDENTIFIER ON
-GO
-
-CREATE TABLE [dbo].[Student] (
-    [StudentID]   INT            IDENTITY (1, 1) NOT NULL,
-    [FirstName]   NVARCHAR (MAX) NOT NULL,
-    [LastName]    NVARCHAR (MAX) NOT NULL,
-    [Email]       NVARCHAR (MAX) NOT NULL,
-    [PhoneNumber] NVARCHAR (MAX) NOT NULL
-);
-
-SELECT
-first_name,
-last_name 
-
-FROM 
-students 
-
-ORDER BY
-first_name;
+﻿SET IDENTITY_INSERT [dbo].[Student] ON
+INSERT INTO [dbo].[Student] ([StudentID], [FirstName], [LastName], [Email], [PhoneNumber]) VALUES (1, N'Melissa', N'Manikam', N'mel.manikam@gmail.com', N'0211098897')
+INSERT INTO [dbo].[Student] ([StudentID], [FirstName], [LastName], [Email], [PhoneNumber]) VALUES (2, N'Sujal', N'Chad', N'sujalchad@gmail.com', N'0273253466')
+INSERT INTO [dbo].[Student] ([StudentID], [FirstName], [LastName], [Email], [PhoneNumber]) VALUES (3, N'Winnie', N'Hok', N'winniehok@gmail.com', N'0278279273')
+INSERT INTO [dbo].[Student] ([StudentID], [FirstName], [LastName], [Email], [PhoneNumber]) VALUES (4, N'Sophie', N'Howaick', N'sophiehowaick@yahoo.com', N'0220263712')
+INSERT INTO [dbo].[Student] ([StudentID], [FirstName], [LastName], [Email], [PhoneNumber]) VALUES (5, N'Celine', N'Dion', N'celinedion@hotmail.com', N'0278782962')
+INSERT INTO [dbo].[Student] ([StudentID], [FirstName], [LastName], [Email], [PhoneNumber]) VALUES (6, N'Jared', N'Chang', N'jaredchang@avcol.school.nz', N'0237382638')
+INSERT INTO [dbo].[Student] ([StudentID], [FirstName], [LastName], [Email], [PhoneNumber]) VALUES (7, N'Tony', N'Smith', N'tonysmith@gmail.com', N'0226375372')
+SET IDENTITY_INSERT [dbo].[Student] OFF
