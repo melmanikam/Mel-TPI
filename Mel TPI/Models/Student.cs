1a6acbbb3d1ca571e8ddc9586cdd506366c00588
Mel Manikam <ac111953@avcol.school.nz>
2023-10-18 12:31:34 +1300

    fixed footer positioning, added spacing for coloumn titles and made drop down options when creating lessons

--- Mel TPI/Models/Student.cs
+++ Mel TPI/Models/Student.cs
@@ -1,13 +1,19 @@
-﻿namespace Mel_TPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Mel_TPI.Models
 {
     public class Student
     {
         public int StudentID { get; set; }
+        [Display(Name = "First Name")]                        
         public string FirstName { get; set; }
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
         public string Email { get; set; }
+        [Display(Name = "Phone Number")]
         public string PhoneNumber { get; set; }
-
+       
         public ICollection<Lesson> Lessons { get; set; }
     }
 }
+
--- Mel TPI/Models/Teacher.cs
+++ Mel TPI/Models/Teacher.cs
@@ -1,12 +1,18 @@
-﻿namespace Mel_TPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Mel_TPI.Models
 {
     public class Teacher
     {
         public int TeacherID { get; set; }
+        [Display(Name = "First Name")]
         public string FirstName { get; set; }
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
         public string Email { get; set; }
+        [Display(Name = "Phone Number")]
         public int PhoneNumber { get; set; }
+        [Display(Name = "Phone Number")]
 
         public ICollection<Lesson> Lessons { get; set; }
     }
